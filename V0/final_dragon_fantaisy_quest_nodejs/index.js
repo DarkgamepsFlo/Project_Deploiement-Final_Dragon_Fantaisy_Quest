e7462abcac3952fe08f6e0b5add50fb0c1b34f73
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2023-12-12 22:54:09 +0100

    Create Player / Enemy
    Ajout des points de vie du joueur et de l'ennemi ainsi que des boutons permettant de décrémenter leurs compteurs de point de vie.

--- V0/final_dragon_fantaisy_quest_nodejs/index.js
+++ V0/final_dragon_fantaisy_quest_nodejs/index.js
@@ -1,26 +1,10 @@
-const cors = require('cors');
-const express = require('express');
-const app = express();
+const app = require('./routes/index');
 const port = 3000;
 
-// Utilisez le middleware cors avec l'origine autorisée
-const corsOptions = {
-  origin: 'http://localhost:8080',
-};
+startServer()
 
-app.use(cors(corsOptions));
-
-// Route principale
-app.get('/', (req, res) => {
-  res.send('Bienvenue sur votre serveur !');
-});
-
-// Route pour une API simple
-app.get('/api/message', (req, res) => {
-  res.json({ message: 'Ceci est un message de l\'API' });
-});
-
-// Démarrez le serveur
-app.listen(port, () => {
-  console.log(`Le serveur écoute sur le port ${port}`);
-});
+function startServer() {
+  app.listen(port, () => {
+    console.info(`Example app listening on port ${port}`)
+  })
+}
