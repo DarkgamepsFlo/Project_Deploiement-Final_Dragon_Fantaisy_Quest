6fc1cc845e29f9cd796d95233280e4a4b15c4c49
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2024-01-29 19:53:53 +0100

    Copy V0 in V1 Folder
    Copie de la V0 dans le dossier V1 pour pouvoir commencer à faire des modifications dans une nouvelle branche.

---
+++ V1/final_dragon_fantaisy_quest_vuejs/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App !!!!!!!!!!!!!!!!!"/> -->
+  <Message></Message>
+  <PointsDeVie></PointsDeVie>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+import Message from './components/Message.vue';
+import PointsDeVie from './components/PointsDeVie.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Message,
+    PointsDeVie
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ V1/final_dragon_fantaisy_quest_vuejs/src/components/Message.vue
@@ -0,0 +1,36 @@
+<!-- Message.vue -->
+<template>
+  <div>
+    <h1>Message de l'API</h1>
+    <p v-if="apiMessage">{{ apiMessage }}</p>
+    <p v-else>Chargement en cours...</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'MessageComponent',
+  data() {
+    return {
+      apiMessage: null,
+    };
+  },
+  mounted() {
+    // Faites une requête GET vers votre API
+    axios.get('http://localhost:3000/api/message')
+      .then(response => {
+        // Assurez-vous que la réponse est au format JSON et contient la propriété 'data'
+        if (response && response.data) {
+          this.apiMessage = response.data.message;
+        } else {
+          console.error('Réponse de l\'API invalide', response);
+        }
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération du message de l\'API', error);
+      });
+  },
+};
+</script>
