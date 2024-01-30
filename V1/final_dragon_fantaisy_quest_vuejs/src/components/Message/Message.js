9960371c201f269ece753896318d1595d0bd9811
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2024-01-30 12:16:10 +0100

    V1.0 Add Test in VueJS
    Ajout d'un test sur le composant de base pour pouvoir vérifier si tout fonctionne. Il permet de vérifier uniquement un composant n'ayant pas de requête API.

--- V1/final_dragon_fantaisy_quest_vuejs/src/components/Message/Message.js
+++ V1/final_dragon_fantaisy_quest_vuejs/src/components/Message/Message.js
@@ -1,13 +1,3 @@
-<!-- Message.vue -->
-<template>
-  <div>
-    <h1>Message de l'API</h1>
-    <p v-if="apiMessage">{{ apiMessage }}</p>
-    <p v-else>Chargement en cours...</p>
-  </div>
-</template>
-
-<script>
 import axios from 'axios';
 
 export default {
@@ -33,4 +23,3 @@ export default {
       });
   },
 };
-</script>
---
+++ V1/final_dragon_fantaisy_quest_vuejs/src/components/Message/Message.vue
@@ -0,0 +1,11 @@
+<!-- Message.vue -->
+<template>
+  <div>
+    <h1>Message de l'API</h1>
+    <p v-if="apiMessage">{{ apiMessage }}</p>
+    <p v-else>Chargement en cours...</p>
+  </div>
+</template>
+
+<script src="../Message.js"></script>
+<style src="./Message.css"></style>
