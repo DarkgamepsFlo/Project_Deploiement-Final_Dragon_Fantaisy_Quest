6c79bab59a8ee14911c0ef4799c55a5559873f69
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2024-02-13 21:21:07 +0100

    Update visible stats
    Modification de l'emplacement des points de vie en les passant de la fenêtre principale dans la barre d'action

--- V1/final_dragon_fantaisy_quest_vuejs/src/components/BarreAction/BarreAction.js
+++ V1/final_dragon_fantaisy_quest_vuejs/src/components/BarreAction/BarreAction.js
@@ -1,15 +1,65 @@
+import axios from 'axios';
+
 export default {
   name: 'BarreAction',
   data() {
     return {
-      barreAction: null,
+      pointsDeVieBase: null,
+      pointsDeVie: null,
+      localType: this.type,
     };
+  },
+  props: {
+    // Propriété reçue du composant parent, indiquant le type (Joueur ou Ennemi)
+    type: {
+      type: String,
+      required: true,
+    },
+    degat: {
+      type: Number,
+      default: 0,
+    },
+  },
+  mounted() {
+    // Méthode appelée automatiquement après que le composant est monté dans le DOM
+    // Elle effectue une requête à l'API pour récupérer les points de vie initiaux
+    this.fetchPointsDeVie();
+  },
+  watch: {
+    degat: 'handleDegatChange', // Utilise la méthode handleDegatChange pour réagir aux changements
   },
   methods: {
     // Méthode appelée lorsqu'on clique sur le bouton de décrémentation des points de vie
     async decrementerPointsDeVie() {
       // Émission d'un événement personnalisé vers le composant parent avec le type du composant actuel
       this.$emit('decrement', 'Joueur');
+    },
+    async fetchPointsDeVie() {
+      try {
+        // Utilisation d'Axios pour effectuer une requête GET à l'API
+        const response = await axios.get('http://localhost:3000/api/pointsDeVie');
+        // Accès aux données de la réponse et mise à jour des points de vie du composant
+        const data = response.data;
+        this.pointsDeVie = data[this.localType.toLowerCase()];
+        this.pointsDeVieBase = data[this.localType.toLowerCase()];
+      } catch (error) {
+        // Gestion des erreurs lors de la récupération des points de vie
+        console.error('Erreur lors de la récupération des points de vie', error);
+      }
+    },
+    handleDegatChange(newValue) {
+      // Réagit lorsque la propriété degat est modifiée
+      console.log(`Nouveaux dégâts : ${newValue}`);
+      
+      if (this.type === "Joueur"){
+        this.pointsDeVie -= newValue;
+      } else {
+        this.pointsDeVie -= newValue;
+        setTimeout(() => {
+          console.log("je décrémente les PV du joueur");
+          this.decrementerPointsDeVie();
+        }, 5000);
+      }
     },
   },
 };
--- V1/final_dragon_fantaisy_quest_vuejs/src/components/BarreAction/BarreAction.vue
+++ V1/final_dragon_fantaisy_quest_vuejs/src/components/BarreAction/BarreAction.vue
@@ -3,6 +3,8 @@
   <div class="back-barre-action">
     <div class="barre-action barre-life">
       <!-- <h1 class="h1-barre-action">Je suis la barre d'affichage de la vie</h1> -->
+      <p>PV : {{ pointsDeVie }} / {{ pointsDeVieBase }}</p>
+      <p>Dégâts reçus : {{ degat }}</p>
     </div>
     <div class="barre-action barre-attack">
       <!-- <h1 class="h1-barre-action">Je suis la barre d'action</h1> -->
