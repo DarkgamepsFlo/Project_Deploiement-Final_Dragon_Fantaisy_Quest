70752227fd9cf514ea1ba81b56da86f3e3db8da5
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2024-02-15 13:04:48 +0100

    Add more test
    Ajout de plus de tests concernant le composant BarreAction

--- V1/final_dragon_fantaisy_quest_vuejs/src/tests/BarreAction.js
+++ V1/final_dragon_fantaisy_quest_vuejs/src/tests/BarreAction.js
@@ -4,18 +4,18 @@ function decrementerPointsDeVie(name, pointsDeVie, pointsDeVieBase, isButtonDisa
     if (!isButtonDisabled) {
       isButtonDisabled = true; // Désactive le bouton
       if(name == 'Punch'){
-        return { decrement: "decrement", Joueur: "Joueur", damage: 8}
+        return { decrement: "decrement", Joueur: "Joueur", damage: 8, isButtonDisabled: isButtonDisabled}
       } else if(name == 'Magic_Attack'){
-        return { decrement: "decrement", Joueur: "Joueur", damage: 12}
+        return { decrement: "decrement", Joueur: "Joueur", damage: 12, isButtonDisabled: isButtonDisabled}
       } else if(name == 'Heal') {
         if (pointsDeVie > pointsDeVieBase - 5){
           pointsDeVie = pointsDeVieBase;
         } else {
           pointsDeVie += 5
         }
-        return { decrement: "decrement", Joueur: "Joueur", damage: -1}
+        return { decrement: "decrement", Joueur: "Joueur", damage: -1, isButtonDisabled: isButtonDisabled}
       }else {
-        return { decrement: "decrement", Joueur: "Joueur", damage: 0}
+        return { decrement: "decrement", Joueur: "Joueur", damage: 0, isButtonDisabled: isButtonDisabled}
       }
     }
   }
@@ -27,7 +27,7 @@ function fetchPointsDeVie(localType) {
     var pointsDeVie = data[localType.toLowerCase()];
     var pointsDeVieBase = data[localType.toLowerCase()];
 
-    return { pv: pointsDeVie, pvbase: pointsDeVieBase};
+    return { pv: pointsDeVie.pointsDeVie, pvbase: pointsDeVieBase.pointsDeVie};
 }
 
 function handleDegatChange(type, pointsDeVie, newValue) {
@@ -37,7 +37,7 @@ function handleDegatChange(type, pointsDeVie, newValue) {
     } else {
       pointsDeVie -= newValue;
       // setTimeout(() => {
-      const json = decrementerPointsDeVie();
+      const json = decrementerPointsDeVie('Punch', 10, 100, false);
       return { pv: pointsDeVie, decrementer: json };
     }
 }
--- V1/final_dragon_fantaisy_quest_vuejs/src/tests/__tests__/BarreAction.test.js
+++ V1/final_dragon_fantaisy_quest_vuejs/src/tests/__tests__/BarreAction.test.js
@@ -1,13 +1,54 @@
-const { decrementerPointsDeVie } = require("../BarreAction");
+const { decrementerPointsDeVie, fetchPointsDeVie, handleDegatChange } = require("../BarreAction");
 
-const returnDecrementerPointDeVie = require("./returnDecrementerPointsDeVie.json");
+const returnDecrementerPointDeVie_Punch = require("./returnDecrementerPointsDeVie_Punch.json");
+const returnDecrementerPointDeVie_MagicAttack = require("./returnDecrementerPointsDeVie_MagicAttack.json");
+const returnDecrementerPointDeVie_Heal = require("./returnDecrementerPointsDeVie_Heal.json");
+const returnDecrementerPointDeVie_Other = require("./returnDecrementerPointsDeVie_Other.json");
+
+const returnFetchPointDeVie_Joueur = require("./returnFetchPointDeVie_Joueur.json");
+const returnFetchPointDeVie_Ennemi = require("./returnFetchPointDeVie_Ennemi.json");
+
+const returnHandleDegatChange_Joueur = require("./returnHandleDegatChange_Joueur.json");
+const returnHandleDegatChange_Ennemi = require("./returnHandleDegatChange_Ennemi.json");
 
 describe("BarraAction.js", () => {
     // Test pour la fonction decrementerPointsDeVie()
     describe("decrementerPointsDeVie", () => {
-        it("Doit retourner les bonnes informations", () => {
+        it("Doit retourner les bonnes informations pour Punch", () => {
             const res = decrementerPointsDeVie('Punch', 10, 100, false);
-            expect(res).toStrictEqual(returnDecrementerPointDeVie);
+            expect(res).toStrictEqual(returnDecrementerPointDeVie_Punch);
+        }),
+        it("Doit retourner les bonnes informations pour Magick_Attack", () => {
+            const res = decrementerPointsDeVie('Magic_Attack', 12, 100, false);
+            expect(res).toStrictEqual(returnDecrementerPointDeVie_MagicAttack);
+        }),
+        it("Doit retourner les bonnes informations pour Heal", () => {
+            const res = decrementerPointsDeVie('Heal', -1, 100, false);
+            expect(res).toStrictEqual(returnDecrementerPointDeVie_Heal);
+        }),
+        it("Doit retourner les bonnes informations pour Other", () => {
+            const res = decrementerPointsDeVie('Other', 0, 100, false);
+            expect(res).toStrictEqual(returnDecrementerPointDeVie_Other);
+        });
+    }),
+    describe("fetchPointsDeVie", () => {
+        it("Doit mettre les bons PV par rapport au Joueur", () => {
+            const res = fetchPointsDeVie('Joueur');
+            expect(res).toStrictEqual(returnFetchPointDeVie_Joueur);
+        }),
+        it("Doit mettre les bons PV par rapport à l'Ennemi", () => {
+            const res = fetchPointsDeVie('Ennemi');
+            expect(res).toStrictEqual(returnFetchPointDeVie_Ennemi);
+        });
+    }),
+    describe("handleDegatChange", () => {
+        it("Doit avoir les bons PV et décrémenter la bonne chose pour le joueur", () => {
+            const res = handleDegatChange('Joueur', 100, 10);
+            expect(res).toStrictEqual(returnHandleDegatChange_Joueur);
+        }),
+        it("Doit avoir les bons PV et décrémenter la bonne chose pour l'ennemi", () => {
+            const res = handleDegatChange('Ennemi', 50, 8);
+            expect(res).toStrictEqual(returnHandleDegatChange_Ennemi);
         });
     })
 });
