21158e2cd239888da18678a843bf6c8a1eb770ef
Ogès Florian <94926129+DarkgamepsFlo@users.noreply.github.com>
2024-02-21 22:55:32 +0100

    Add Music for Ending
    Ajout de musique lors des écrans de fin

--- V3/final_dragon_fantaisy_quest_vuejs/src/pages/GameOver/GameOver.js
+++ V3/final_dragon_fantaisy_quest_vuejs/src/pages/GameOver/GameOver.js
@@ -7,9 +7,14 @@ export default {
   methods: {
     restartApplication() {
       location.reload();
+    },
+    playGameOverMusic() {
+      const audio = new Audio(require('@/assets/GameOver.mp3'));
+      audio.play();
     }
   },
   mounted() {
+    this.playGameOverMusic();
     setTimeout(() => {
       this.showYouDied = true; // Affichez les éléments après une certaine période
     }, 1000); // Réglez le délai en millisecondes pour afficher les éléments après 1 seconde
--- V3/final_dragon_fantaisy_quest_vuejs/src/pages/VictoryPage/VictoryPage.js
+++ V3/final_dragon_fantaisy_quest_vuejs/src/pages/VictoryPage/VictoryPage.js
@@ -1,7 +1,14 @@
 export default {
+  mounted() {
+    this.playVictoryMusic();
+  },
   methods: {
     restartApplication() {
       location.reload();
+    },
+    playVictoryMusic() {
+      const audio = new Audio(require('@/assets/Fanfare.mp3'));
+      audio.play();
     }
   }
 }
